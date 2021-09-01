da66f806efc176167e92f4537f728f294a88d51c
Maria Mireles <mireles335@gmail.com>
2021-09-01 14:47:29 -0500

    added replies

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,8 +1,8 @@
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
-   // add comment to pizza
-   addComment({ params, body }, res) {
+  // add comment to pizza
+  addComment({ params, body }, res) {
     console.log(body);
     Comment.create(body)
       .then(({ _id }) => {
@@ -21,6 +21,20 @@ const commentController = {
       })
       .catch(err => res.json(err));
   },
+
+  // add reply to comment
+  addReply({ params, body }, res) {
+    Comment.findOneAndUpdate({ _id: params.commentId }, { $push: { replies: body } }, { new: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
+  },
+
   // remove comment
   removeComment({ params }, res) {
     Comment.findOneAndDelete({ _id: params.commentId })
@@ -42,6 +56,17 @@ const commentController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.json(err));
+  },
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
 };
+
 module.exports = commentController;
--- models/pizza.js
+++ models/pizza.js
@@ -1,45 +1,49 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
-  },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal)
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
     comments: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'Comment'
-      }
-    ]
+        ref: "Comment",
+      },
+    ],
   },
   {
     toJSON: {
       virtuals: true,
-      getters: true, 
+      getters: true,
     },
-    id: false
+    id: false,
   }
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
-  });
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
+});
 
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
-//exports the Pizza model 
-module.exports = Pizza; 
+//exports the Pizza model
+module.exports = Pizza;
