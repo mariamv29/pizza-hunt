7a6480520d508b4bdccc1e94fd068d3a8995150c
Maria Mireles <mireles335@gmail.com>
2021-09-02 15:16:26 -0500

    Deploying

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
