8958ca1187ae674128206eec470b0395cb0dfd1f
Maria Mireles <mireles335@gmail.com>
2021-09-02 20:22:15 -0500

    tesing heroku

--- models/Comment.js
+++ models/Comment.js
@@ -2,44 +2,43 @@ const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
-    {
-      // set custom id to avoid confusion with parent comment _id
-      replyId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId()
-      },
-      replyBody: {
-        type: String,
-        required: true
-      },
-      writtenBy: {
-        type: String,
-        required: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-      }
+  {
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
     },
-    {
-      toJSON: {
-        getters: true
-      }
+    replyBody: {
+      type: String,
+      required: true
+    },
+    writtenBy: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
     }
-  );
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
 
 const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
-      required: true, 
-      trim: true
+      required: true
     },
     commentBody: {
       type: String,
-      required: true, 
-      trim: true
+      required: true
     },
     createdAt: {
       type: Date,
--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
-//   useFindAndModify: false,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -18,6 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
+app.use(require('./routes'));
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
-
-
